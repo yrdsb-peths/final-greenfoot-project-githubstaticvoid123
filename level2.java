7372c149c89c0e2d825a6343c4c45d94b4f5ef53
Matthew Li <150149921+githubstaticvoid123@users.noreply.github.com>
2024-01-11 09:09:24 -0500

    Fixed leveling system

---
+++ level2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class level2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class level2 extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class level2.
+     * 
+     */
+    public level2()
+    {
+    }
+}
